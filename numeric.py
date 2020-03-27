71aae1f7ec7e1070ae143d66ea665bf0d83442b5
ra <ra@ra.test>
2020-03-27 02:49:22 +0200

    primes4 is the best for now

--- numeric.py
+++ numeric.py
@@ -44,7 +44,7 @@ def primes1(n_max):
 
         num += 1
 
-# alternative since we use multiplication here, when num is big it is much faster to compute sqrt(num) once than
+# sqrt(num)
 def primes2(n_max):
 
     p, num, cnt = list(), 2, 0
@@ -70,7 +70,39 @@ def primes2(n_max):
 
         num += 1
 
+
 def primes3(n_max):
+    p, num, cnt = list(), 2, 0
+    next_square, sqrt_num = 4, 1
+
+    while cnt < n_max:
+
+        should_add = True
+
+        for prime in p:
+
+            if num % prime == 0:
+                should_add = False
+                break
+
+            if prime > sqrt_num:
+                break
+
+
+        if should_add:
+            p.append(num)
+            cnt += 1
+            yield num
+
+        if num == next_square:
+            next_square += 2 * sqrt_num + 1
+            sqrt_num += 1
+
+        num += 1
+
+
+# no should_add boolean
+def primes4(n_max):
     p, num, cnt = [2], 2, 1
     next_square, sqrt_num = 4, 1
 
@@ -89,42 +121,28 @@ def primes3(n_max):
                 yield num
                 break
 
+        # TODO: analyze situation when num is a square - may it be optimized?
         if num == next_square:
             next_square += 2 * sqrt_num + 1
             sqrt_num += 1
 
 
-
-def primes22(n_max):
-    p, num, cnt = list(), 2, 0
-    next_square, sqrt_num = 4, 1
-
-    while cnt < n_max:
-
-        should_add = True
-
-        for prime in p:
-
-            # changing order of branches
-
-            if prime > sqrt_num:
-                break
-
-            if num % prime == 0:
-                should_add = False
-                break
-
-        if should_add:
-            p.append(num)
-            cnt += 1
-            yield num
-
-        if num == next_square:
-            next_square += 2 * sqrt_num + 1
-            sqrt_num += 1
-
-        num += 1
+primes = primes4
 
 # factor function
 def factor(n):
-    pass
+    result, current = list(), n
+
+    for prime in primes(n):
+        if prime > current:
+            break
+
+        counter = 0
+        while current % prime == 0:
+            current //= prime
+            counter += 1
+
+        if counter > 0:
+            result.append((prime, counter))
+
+    return result
